fdf698b0be1011b204e1b162bd5578c3d73cd183
s.y.s <s.y.s.sam.sys@gmail.com>
2019-03-28 14:44:12 +0800

    first commit

---
+++ paginator.go
@@ -0,0 +1,149 @@
+package paginator
+
+import (
+	"encoding/base64"
+	"errors"
+	"strconv"
+	"strings"
+)
+
+type Paginator struct {
+	cursorPrefix string
+	first        *int
+	last         *int
+	after        *string
+	before       *string
+	total        int
+	limit        int
+	offset       int
+	from         int
+	to           int
+}
+
+func NewPaginator(cursorPrefix string, first *int, last *int, after *string, before *string, skip *int, total int) (*Paginator, error) {
+	var p = Paginator{
+		cursorPrefix: cursorPrefix,
+		first:        first,
+		last:         last,
+		after:        after,
+		before:       before,
+		total:        total,
+		from:         0,
+		to:           0,
+		limit:        0,
+		offset:       0,
+	}
+	var err error
+	if first != nil && last != nil {
+		return nil, errors.New("Passing both `first` and `last` is not supported.")
+	}
+
+	if first != nil && *first > 100 {
+		return nil, errors.New("Requesting " + strconv.Itoa(*first) + " records on the connection exceeds the `first` limit of 100 records.")
+	}
+
+	if last != nil && *last > 100 {
+		return nil, errors.New("Requesting " + strconv.Itoa(*last) + " records on the connection exceeds the `last` limit of 100 records.")
+	}
+
+	if after != nil && *after != "" {
+		p.from, err = DecodeCursor(p.cursorPrefix, *before)
+		if err != nil {
+			return nil, errors.New("`" + *before + "` does not appear to be a valid cursor.")
+		}
+		p.from++
+		p.from = Min(p.from, total-1)
+	}
+
+	if before != nil && *before != "" {
+		p.to, err = DecodeCursor(p.cursorPrefix, *before)
+		if err != nil {
+			return nil, errors.New("`" + *before + "` does not appear to be a valid cursor.")
+		}
+		p.to--
+		p.to = Min(p.to, 0)
+	}
+
+	if first != nil {
+		p.to = Min(p.from+*first, total-1)
+	}
+
+	if last != nil {
+		p.from = Max(p.to-*last, 0)
+	}
+
+	if skip != nil {
+		p.from = p.from - *skip
+	}
+
+	p.offset = p.from
+	if p.to >= p.from {
+		p.limit = p.to - p.from
+	}
+
+	return &p, nil
+}
+
+func (p Paginator) From() int {
+	return p.from
+}
+
+func (p Paginator) To() int {
+	return p.to
+}
+
+func (p Paginator) Limit() int {
+	return p.limit
+}
+
+func (p Paginator) Offset() int {
+	return p.offset
+}
+
+func (p Paginator) HasNextPage() bool {
+	if p.total > p.to {
+		return true
+	}
+	return false
+}
+
+func (p Paginator) HasPreviousPage() bool {
+	if p.from > 0 {
+		return true
+	}
+	return false
+}
+
+// EncodeCursor the cursot position in base64
+func EncodeCursor(cursorPrefix string, index int) string {
+	return base64.StdEncoding.EncodeToString([]byte(cursorPrefix + strconv.Itoa(index)))
+}
+
+// DecodeCursor decodes the base 64 encoded cursor and resturns the integer
+func DecodeCursor(cursorPrefix string, cursor string) (int, error) {
+	c, err := base64.StdEncoding.DecodeString(cursor)
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := strconv.Atoi(strings.TrimPrefix(string(c), cursorPrefix))
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
---
+++ paginator_test.go
@@ -0,0 +1,76 @@
+package paginator
+
+import (
+	"testing"
+)
+
+func TestFieldsRequired(t *testing.T) {
+	/*
+		case 1:
+		$paginator = new Paginator($first: 100, $last: null, $after: null, $before: null, $total: 1000);
+		$result: select * from testing limit 100
+
+		case 2:
+		$paginator = new Paginator($first: 0, $last: 100, $after: null, $before: null, $total: 1000);
+		$result: select * from testing limit 900, 1000
+
+		case 3
+		$paginator = new Paginator($first: 100, $last: null, $after: 100, $before: null, $total: 1000);
+		$result: select * from testing limit 101, 201
+
+		case 4
+		$paginator = new Paginator($first: null, $last: 100, $after: null, $before: 100, $total: 1000);
+		$result: select * from testing limit 0, 99
+
+		case 5
+		$paginator = new Paginator($first: 100, $last: null, $after: 50, $before: 80, $total: 1000);
+		$result: select * from testing limit 51, 79
+
+		case 6
+		$paginator = new Paginator($first: 100, $last: null, $after: 50, $before: 180, $total: 1000);
+		$result: select * from testing limit 51, 151
+
+		case 7
+		$paginator = new Paginator($first: null, $last: 100, $after: 50, $before: 80, $total: 1000);
+		$result: select * from testing limit 819, 919
+
+		case 8
+		$paginator = new Paginator($first: null, $last: 100, $after: 880, $before: 80, $total: 1000);
+		$result: select * from testing limit 880, 919
+	*/
+	number100 := 100
+	var tests = []struct {
+		first  *int
+		last   *int
+		after  string
+		before string
+		skip   int
+		total  int
+		limit  int
+		offset int
+		from   int
+		to     int
+	}{
+		{&number100, nil, "", "", 0, 1000, 100, 0, 0, 99},
+	}
+
+	for i, test := range tests {
+		paginator, err := NewPaginator("cursor", test.first, test.last, &test.after, &test.before, &test.skip, test.total)
+		if err != nil {
+			t.Errorf("Got Error on NewPaginator(%q): %s", test, err)
+		} else {
+			if paginator.Limit() != test.limit {
+				t.Errorf("Got Error on case %d limit doest not match %d : %d", i, test.limit, paginator.Limit())
+			}
+			if paginator.Offset() != test.offset {
+				t.Errorf("Got Error on case %d offset doest not match %d : %d", i, test.offset, paginator.Offset())
+			}
+			if paginator.From() != test.from {
+				t.Errorf("Got Error on case %d from doest not match %d : %d", i, test.from, paginator.From())
+			}
+			if paginator.To() != test.to {
+				t.Errorf("Got Error on case %d to doest not match %d : %d", i, test.to, paginator.To())
+			}
+		}
+	}
+}
